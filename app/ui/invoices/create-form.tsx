e763e1b730567d9c7b00cc4ddf4bc0c795fae04c
Fani Triastowo <fani.triastowo@gmail.com>
2024-12-02 13:45:38 +0700

    invoice: edit-form validation

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -43,19 +43,16 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
-          {
-            state.errors?.customerId && (
-              <div id="customer-error" aria-live="polite" aria-atomic="true">
-                {
-                  state.errors.customerId.map((error: string) => (
-                    <p className="mt-2 text-sm text-red-500" key={error}>
-                      {error}
-                    </p>
-                  ))
-                }
-              </div>
-            )
-          }
+          <div id="customer-error" aria-live="assertive" aria-atomic="true">
+            {
+              state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))
+            }
+          </div>
         </div>
 
         {/* Invoice Amount */}
@@ -76,19 +73,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
-            {
-              state.errors?.amount && (
-                <div id="amount-error" aria-live="polite" aria-atomic="true">
-                  {
-                    state.errors.amount.map((error: string) => (
-                      <p className="mt-2 text-sm text-red-500" key={error}>
-                        {error}
-                      </p>
-                    ))
-                  }
-                </div>
-              )
-            }
+            <div id="amount-error" aria-live="assertive" aria-atomic="true">
+              {state.errors?.amount &&
+                state.errors.amount.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
         </div>
 
@@ -131,23 +123,25 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
             <div>
-              {
-                state.errors?.status && (
-                  <div id="status-error" aria-live="polite" aria-atomic="true">
-                    {
-                      state.errors.status.map((error: string) => (
-                        <p className="mt-2 text-sm text-red-500" key={error}>
-                          {error}
-                        </p>
-                      ))
-                    }
-                  </div>
-                )
-              }
+              <div id="status-error" aria-live="assertive" aria-atomic="true">
+                {state.errors?.status &&
+                  state.errors.status.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                      {error}
+                    </p>
+                  ))}
+              </div>
 
             </div>
           </div>
         </fieldset>
+
+        <div aria-live="polite" aria-atomic="true">
+          {state.message ? (
+            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+          ) : null}
+        </div>
+
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
